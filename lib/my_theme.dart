5d996e208e92b787c29c20cbdd57628265105ec2
Rashid Saleem <mrashidcit@gmail.com>
2022-12-17 12:48:56 +0500

    Complete Funcationlity

---
+++ lib/my_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class Mytheme {
+  static Color primaryColor = Color.fromARGB(255, 93, 176, 117);
+
+  static Color textFieldBackgroundColor = Color.fromRGBO(246, 246, 246, 1);
+}
